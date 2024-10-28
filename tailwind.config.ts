dfece5071fa027fc10ed8e30089a9b2f409f6e12
Atakan Temizkan <atikotemizkan@gmail.com>
2024-10-28 14:20:00 +0300

    Add PostCSS and Tailwind configuration

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "lofi", "black"],
+  },
+};
+
+export default config;
