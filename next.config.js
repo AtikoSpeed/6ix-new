5c05cfe0e2350a1c093b181b4263f918c0a53171
Atakan Temizkan <atikotemizkan@gmail.com>
2024-10-29 09:15:00 +0300

    Add image optimization config

--- next.config.js
+++ next.config.js
@@ -3,10 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false, // Changed to false to catch type errors
   },
   experimental: {
     typedRoutes: true,
+  },
+  images: {
+    domains: ["img.daisyui.com"], // Add daisyUI image domain
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
 };
 
