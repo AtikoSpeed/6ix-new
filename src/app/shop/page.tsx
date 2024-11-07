d2dbf9ebf7dfc6235af4efaf6c29bbbb85a1ed8a
Atakan Temizkan <atikotemizkan@gmail.com>
2024-11-07 15:45:00 +0300

    Add shop page

---
+++ src/app/shop/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ShopPage() {
+  return (
+    <div className="col-span-12 row-span-11 flex justify-center items-center">
+      <h1 className="text-4xl font-bold">Welcome to 6ixarchive</h1>
+    </div>
+  );
+}
